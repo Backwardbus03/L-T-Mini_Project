41f0951138fef3b517bdee5d9afc70cb2b305c0e
Backwardbus03 <backwardbus03@gmail.com>
2025-06-12 23:41:07 +0530

    Finished making models

--- bookmarks/models.py
+++ bookmarks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from accounts.models import CustomUser
+from jobs.models import Job
 
 # Create your models here.
+class JobBookmark(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='bookmarks')
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name='bookmarks')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'job')
+
+    def __str__(self):
+        return f"{self.user.user.username} bookmarked {self.job.title}"
