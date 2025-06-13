a5cc81d6dc750e7daf2185bba45b6363aa746d2d
Sumedh Hadkar <90079272+Backwardbus03@users.noreply.github.com>
2025-06-13 16:37:14 +0530

    Delete job directory

--- job/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class JobsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'job'
-
-    def ready(self):
-        import job.signals
--- job/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.job_list, name='job_list'),
-    path('job/<slug:slug>/', views.job_detail, name='job_detail'),
-    path('bookmark/<int:job_id>/', views.toggle_bookmark, name='toggle_bookmark'),
-    path('my-bookmarks/', views.user_bookmarks, name='user_bookmarks'),
-]
